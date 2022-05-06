2697c2d34466866573131d806a92ea1681825e9c
Rezilay <the.rezilay@gmail.com>
2022-05-06 19:51:13 +0430

    getting apis and creating number formatter

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../navbar/Navbar";
+import { abbrNum } from "../numberFormat";
 import "./Home.css";
 
 const Home = () => {
-  return <div className="home__main"></div>;
+  const [current, setCurrent] = useState("Iran");
+  const [info, setInfo] = useState({});
+
+  useEffect(() => {
+    let api;
+    if (current === "WorldWide") {
+      api = "https://disease.sh/v3/covid-19/all";
+      fetch(api)
+        .then((res) => res.json())
+        .then((data) => setInfo(data));
+    }
+
+    if (current !== "WorldWide") {
+      api = `https://disease.sh/v3/covid-19/countries/${current}`;
+      fetch(api)
+        .then((res) => res.json())
+        .then((data) => setInfo(data));
+    }
+  }, [current]);
+
+  return (
+    <div className="home__main">
+      
+    </div>
+  );
 };
 
 export default Home;
