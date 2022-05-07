9217820ddc115e8bf7880b234d1994409220b1da
Rezilay <the.rezilay@gmail.com>
2022-05-07 09:11:21 +0430

    created table with country list

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -6,6 +6,14 @@ import "./Home.css";
 const Home = () => {
   const [current, setCurrent] = useState("Iran");
   const [info, setInfo] = useState({});
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    let api = "https://corona.lmao.ninja/v3/covid-19/countries";
+    fetch(api)
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
 
   useEffect(() => {
     let api;
@@ -24,9 +32,37 @@ const Home = () => {
     }
   }, [current]);
 
+
   return (
     <div className="home__main">
-      
+      <div className="home__container">
+        <div className="home__country"></div>
+        <div className="home__table">
+          <input
+            type="text"
+            className="home__myInput"
+            // onKeyUp={2 + 2}
+            placeholder="Search for names.."
+          />
+          <table className="home__table">
+            <thead>
+              <tr className="home__header">
+                <th style={{ width: "60%" }}>Country</th>
+                <th style={{ width: "40%" }}>Cases</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {countries.map((c) => (
+                <tr key={c.country} onClick={() => setCurrent(c.country)}>
+                  <td>{c.country}</td>
+                  <td>{c.cases}</td>{" "}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 };
